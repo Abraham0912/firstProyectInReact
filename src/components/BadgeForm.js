c40d16902cfa76b90e26d0c1e50d05f4e4f4336f
Abraham0912 <09120912ahn@gmail.com>
2021-02-07 21:42:23 -0600

    addBadgeForm.js

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+import './styles/BadgeForm.css'
+
+export default class BadgeForm extends Component {
+    /* handleChange = (e) =>{
+        // console.log(e);
+        // console.log({name: e.target.name});
+        // console.log({value: e.target.value});
+        this.setState({[e.target.name]:e.target.value,});
+    } */
+
+    handleClick = (e) =>{
+        console.log("Button was click");
+        console.log(this.state);
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+    render() {
+        return (
+            <div className="BadgeForm_1">
+                <h1>New user</h1>
+                <form onSubmit={this.handleSubmit} autoComplete="off">
+                    <div>
+                        <label>First name</label>
+                        <input onChange={this.props.onChange} className="form-control" name="firstName" type="text" value={this.props.firstName}/>
+                        <label>Last name</label>
+                        <input onChange={this.props.onChange} className="form-control" name="lastName" type="text" value={this.props.lastName}/>
+                        <label>Email</label>
+                        <input onChange={this.props.onChange} className="form-control" name="email" type="email" value={this.props.email}/>
+                        <label>Job title</label>
+                        <input onChange={this.props.onChange} className="form-control" name="jobTitle" type="text" value={this.props.jobTitle}/>
+                        <label>Twiter</label>
+                        <input onChange={this.props.onChange} className="form-control" name="twiter" type="text" value={this.props.twiter}/>
+                        <br/>
+                        <button onClick={this.handleClick} className="btn btn-primary">Aceptar</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
