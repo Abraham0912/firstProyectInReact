ed16ad33c9e8b94f46e722683d371ef79393dcee
Abraham0912 <09120912ahn@gmail.com>
2021-02-07 21:36:54 -0600

    Creando carpeta components y componente App.js

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+/* import BadgeNew from '../pages/BadgeNew'
+import Badges from '../pages/Badges' */
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Abraham h</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-
+//ESTILOS
 import './global.css';
+//COMPONENTES
+import App from './components/App'
 
 
 const container = document.getElementById('app');
 
-ReactDOM.render("hopla", container);
+ReactDOM.render("Abraham Hernandez", container);
