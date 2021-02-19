79ec4a78cc96677a2c98e7685282254edf711c76
Abraham0912 <09120912ahn@gmail.com>
2021-02-19 09:06:30 -0600

    Creando el componenete Layaut.js

---
+++ src/components/Layaut.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react'
+import PropTypes from 'prop-types'
+
+function Layaut(props) {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+
+export default Layaut
+
