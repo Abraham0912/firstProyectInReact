ca6fb7ee833c59eeb8dabc937e7917ddfca4da7d
Abraham0912 <09120912ahn@gmail.com>
2021-02-24 12:00:49 -0600

    Add badges and badgeNew on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 import './styles/Navbar.css'
-import img1 from '../images/logo.svg'
+import img1 from '../images/arla-logo.png'
 
 class Navbar extends Component {
     render() {
         return (
             <div className="Navbar">
-                <div>
+                
                     <Link className="Navbar__brand" to="/">
                         <img className="Navbar__brand-logo" src={img1} alt="alienxD"/>
-                        <span className="Navbar__title_1">Platzi</span>
-                        <span className="Navbar__title_2">Conf</span>
+                        <span className="Navbar__title_1">Swamp</span>
+                        <span className="Navbar__title_2">Per</span>
+                        
                     </Link>
+
+
+
+                <div className="Navbar__brand2">
+                    <Link className="nav-link" to="/badges">Badges</Link>
+                    <Link className="" to="/badge/new">BadgesNew</Link>
+
+
                 </div>
                 
             </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Home(props) {
+    return (
+        <div>
+            <h1>Soy home page.js</h1>
+        </div>
+    )
+}
+
+export default Home
