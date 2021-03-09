98f49ec5046125fc672c29bdcc538f2f9c721dc3
Abraham0912 <09120912ahn@gmail.com>
2021-03-09 13:50:09 -0600

    add metod sumar on Pruebas.js

--- src/pages/Pruebas.js
+++ src/pages/Pruebas.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Fragment , Component } from 'react'
 
 export default class componentName extends Component {
+
+    sumar = ()=>{
+        alert("Estoy sumando xD");
+    }
+
     render() {
         return (
+            <Fragment>
             <div>
                 <h1>Componente Pruebas.js</h1>
+                <button onClick={this.sumar}>Sumar</button>
+                <button>Restar</button>
             </div>
+            </Fragment>
         )
     }
 }
