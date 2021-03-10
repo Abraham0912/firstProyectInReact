a88122fb441bad627bb2b6cb3ad5519f4d6a2314
Abraham0912 <09120912ahn@gmail.com>
2021-03-09 21:31:12 -0600

    realizando la peticion a una api con fetch

--- src/components/pruebas/PeticionesAsincronasAJAXAPIs.js
+++ src/components/pruebas/PeticionesAsincronasAJAXAPIs.js
@@ -9,13 +9,51 @@ function Pokemon(props){
 }
 export default class PeticionesAsincronas extends Component {
     state = {
-        pokemon:[],
+        pokemons:[],
+        contenido: false,
+    }
+
+    componentDidMount(){
+        //Peticion asincrona
+        let url = "https://pokeapi.co/api/v2/pokemon";
+        fetch(url)
+        .then(res => res.json())
+        .then(json =>{
+            console.log("Soy el metodo componentDidMouunt()");
+            console.log(json);
+            json.results.forEach(value=>{
+                fetch(value.url)
+                .then((res)=>res.json())
+                .then((json)=>{
+                    console.log(json);
+                    let pokemon = {
+                        id: json.id,
+                        name: json.name,
+                        avatar: json.sprites.front_default,
+                    };
+
+                    let pokemons = [...this.state.pokemons, pokemon];
+                    this.setState({pokemons});
+                })
+            })
+        })
+        //Peticion asincrona_ FIN
+    }
+
+    verContenido = ()=>{
+        this.setState({contenido:true,})
+    }
+    hideContenido = ()=>{
+        this.setState({contenido:false,})
     }
 
     render() {
         return (
             <div>
                 <h1>Componente: PeticionesAsíncronas(AJAX y API's)</h1>
+                
+                
+                
             </div>
         )
     }
