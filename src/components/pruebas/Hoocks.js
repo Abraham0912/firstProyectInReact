d1c43c92cc2ef81765758dd30091565b3d13d023
Abraham0912 <09120912ahn@gmail.com>
2021-03-10 10:47:06 -0600

    Agregando metodos sumar,restar y saludar

--- src/components/pruebas/Hoocks.js
+++ src/components/pruebas/Hoocks.js
@@ -1,9 +1,26 @@
 import React, {useState} from 'react'
 
-export default function Hoocks() {
+export default function Hoocks(props) {
+    const [contador, setContador] = useState(0);
+
+    const sumar  = ()=>{setContador(contador+1);}
+
+    const restar  = ()=>{setContador(contador-1);}
+
+    const saludar  = ()=>{alert("hola buen dia, desde hocks");}
+
     return (
         <div>
-            <h1>Hoocks</h1>
+            <h1>Hoocks - useState</h1>
+            <h2>Subtitulo:{props.titulo}</h2>
+            <p>{contador}</p>
+            <nav>
+                <button onClick={restar}>Restar</button>
+                <button onClick={sumar}>Sumar</button>
+                <button onClick={saludar}>Saludar</button>
+            </nav>
         </div>
     )
 }
+
+
