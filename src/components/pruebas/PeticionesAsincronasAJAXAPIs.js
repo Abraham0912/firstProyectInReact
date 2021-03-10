57f5e193403b87d2851d0725e3769e4f4630171f
Abraham0912 <09120912ahn@gmail.com>
2021-03-09 20:50:48 -0600

    Creando componenete Pokemon dentro del archivo PeticionesAsincronas.js

--- src/components/pruebas/PeticionesAsincronasAJAXAPIs.js
+++ src/components/pruebas/PeticionesAsincronasAJAXAPIs.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
-
+function Pokemon(props){
+    return(
+        <figure>
+            <img src={props.avatar} alt={props.name}/>
+            <figcaption>{props.name}</figcaption>
+        </figure>
+    )
+}
 export default class PeticionesAsincronas extends Component {
+    state = {
+        pokemon:[],
+    }
+
     render() {
         return (
             <div>
-                <h1>PeticionesAsíncronas(AJAX y API's)</h1>
+                <h1>Componente: PeticionesAsíncronas(AJAX y API's)</h1>
             </div>
         )
     }
